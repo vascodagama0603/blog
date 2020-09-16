d6f64b3bb4304fa312b36e0ab70ea003ffd218a8
unknown <ym19900603.2017@gmail.com>
2020-09-17 05:11:05 +0900

    adsence

--- src/components/common/meta/ArticleMeta.js
+++ src/components/common/meta/ArticleMeta.js
@@ -63,6 +63,7 @@ const ArticleMetaGhost = ({ data, settings, canonical }) => {
             <Helmet>
                 <title>{ghostPost.meta_title || ghostPost.title}</title>
                 <meta name="google-site-verification" content="oQyxciWFA4tCe5IMLGhoT22YX5do2MZa-0N4u2ETUaY" />
+                <script data-ad-client="ca-pub-9180260030467303" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                 <meta name="description" content={ghostPost.meta_description || ghostPost.excerpt} />
                 <link rel="canonical" href={canonical} />
 
