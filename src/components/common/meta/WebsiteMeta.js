208dd64a8fcf8d292ba61569051b828ef17b5b85
unknown <ym19900603.2017@gmail.com>
2020-09-18 02:35:13 +0900

    vvbb

--- src/components/common/meta/WebsiteMeta.js
+++ src/components/common/meta/WebsiteMeta.js
@@ -66,14 +66,6 @@ const WebsiteMeta = ({ data, settings, canonical, title, description, image, typ
                 {settings.twitter && <meta name="twitter:creator" content={settings.twitter} />}
                 <script type="application/ld+json">{JSON.stringify(jsonLd, undefined, 4)}</script>
                 <script data-ad-client="ca-pub-9180260030467303" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107259287-5"></script>
-                <script>
-                var dataLayer = window.dataLayer =  window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments)}
-                gtag('js', new Date());
-
-                gtag('config', 'UA-107259287-5');
-                </script>
             </Helmet>
             <ImageMeta image={shareImage} />
         </>
