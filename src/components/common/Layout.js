7229d3b51565491b21dd4d38bea1f6ff097a085a
unknown <ym19900603.2017@gmail.com>
2020-09-18 03:00:07 +0900

    aaaa

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -28,6 +28,8 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
             <Helmet>
                 <html lang={site.lang} />
                 <style type="text/css">{`${site.codeinjection_styles}`}</style>
+                <script data-ad-client="ca-pub-9180260030467303" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
                 <body className={bodyClass} />
             </Helmet>
 
--- src/components/common/meta/WebsiteMeta.js
+++ src/components/common/meta/WebsiteMeta.js
@@ -52,7 +52,6 @@ const WebsiteMeta = ({ data, settings, canonical, title, description, image, typ
             <Helmet>
                 <title>{title}</title>
                 <meta name="google-site-verification" content="oQyxciWFA4tCe5IMLGhoT22YX5do2MZa-0N4u2ETUaY" />
-                <script data-ad-client="ca-pub-9180260030467303" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                 <meta name="description" content={description} /> 
                 <link rel="canonical" href={canonical} />
                 <meta property="og:site_name" content={settings.title} />
